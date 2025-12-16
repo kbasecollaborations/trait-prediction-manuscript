e68a4d733353a4c8ae9dd6f76919dcdcb54cb928
Dileep Kishore <k.dileep1994@gmail.com>
2025-12-16 10:57:08 -0500

    chore: write scripts to run data and plotting scripts

---
+++ run_data_scripts.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+python -m scripts.figure1.figure1b_data.py
+
+python -m scripts.figure2.figure2a_data.py
+python -m scripts.figure2.figure2b_data.py
+
+python -m scripts.figure3.figure3a_gapmind_random_split.py
+python -m scripts.figure3.figure3a_gapmind_dataset_split.py
+python -m scripts.figure3.figure3ab_data.py
+python -m scripts.figure3.figure3c_data.py
+
+python -m scripts.figure4.figure4c_data.py
+
+python -m scripts.figure5.figure5a_data.py
+python -m scripts.figure5.figure5b_data.py
+python -m scripts.figure5.figure5cd_data.py
---
+++ run_figure_scripts.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# python -m scripts.figure1.figure1b_plot.py
+python -m scripts.figure1.figure2_plot.py
+python -m scripts.figure1.figure3_plot.py
+python -m scripts.figure1.figure4_plot.py
+python -m scripts.figure1.figure5_plot.py
